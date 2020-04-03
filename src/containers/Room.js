7eeceb2ead1ad1d50835760d4a8fa42527d98960
Greg Santos <gregsantos@gmail.com>
2020-04-03 15:54:31 -0700

    Update room grid and aside flex

--- src/containers/Room.js
+++ src/containers/Room.js
@@ -5,12 +5,6 @@ import { FaUserSecret } from 'react-icons/fa'
 import { P, H1, Button, Input, Form, RoomWrapper } from '../components'
 import '../css/cards.css'
 
-const members = [
-  { id: 1, title: 'Score', date: '010120', excerpt: 'Lorum Ips...' },
-  { id: 1, title: 'Bid', date: '010120', excerpt: 'Lorum Ips...' },
-  { id: 1, title: 'Bidder', date: '010120', excerpt: 'Lorum Ips...' },
-]
-
 export const Container = props => (
   <div
     {...props}
@@ -29,14 +23,14 @@ const RoomPage = () => (
         // use arrays for mobile-first responsive styles
         height: '100%',
         gridTemplateColumns: [
-          'auto', // default to a stacked layout on small screens
-          '3fr minmax(320px, 1fr)', // use columns for larger screens
+          'auto',
+          '1', // default to a stacked layout on small screens
+          '3fr 1fr', // use columns for larger screens
         ],
       }}
     >
       <main
         sx={{
-          height: '100%',
           display: 'grid',
           gridGap: 2, // theme.space[4]
           // use arrays for mobile-first responsive styles
@@ -52,13 +46,13 @@ const RoomPage = () => (
           },
         }}
       >
-        <div>
+        <div sx={{ justifySelf: 'start', alignSelf: 'start' }}>
           <Container>
             <FaUserSecret size='75%' />
           </Container>
         </div>
         <div />
-        <div>
+        <div sx={{ justifySelf: 'end', alignSelf: 'start' }}>
           <Container>
             <FaUserSecret size='75%' />
           </Container>
@@ -86,62 +80,15 @@ const RoomPage = () => (
         </div>
       </main>
 
-      <aside sx={{ display: 'flex', flexDirection: 'column' }}>
+      <aside
+        sx={{
+          backgroundColor: 'black',
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
         <Container>
-          <ul
-            sx={{
-              listStyle: 'none',
-              m: 0,
-              px: 3,
-              py: 4,
-            }}
-          >
-            {members.map(member => (
-              <li
-                key={member.id}
-                sx={{
-                  mb: 4,
-                }}
-              >
-                <Styled.h2
-                  sx={{
-                    m: 0,
-                  }}
-                >
-                  <a
-                    href={member.slug}
-                    sx={{
-                      color: 'inherit',
-                      textDecoration: 'none',
-                      ':hover,:focus': {
-                        color: 'muted',
-                        textDecoration: 'underline',
-                      },
-                    }}
-                  >
-                    {member.title}
-                  </a>
-                </Styled.h2>
-                <small sx={{ fontWeight: 'bold' }}>{member.date}</small>
-                <Styled.p>{member.excerpt}</Styled.p>
-              </li>
-            ))}
-          </ul>
-        </Container>
-        <Container>
-          <div
-            sx={{
-              height: 'inherit',
-              display: 'grid',
-              gridGap: 1, // theme.space[4]
-              // use arrays for mobile-first responsive styles
-              gridTemplateColumns: [
-                'auto', // default to a stacked layout on small screens
-                '1fr 1fr 1fr', // use columns for larger screens
-              ],
-              '& :nth-child(odd)': {},
-            }}
-          >
+          <div>
             {['♠A', '♠K', '♦A', '♣J', '♣06', '♥09'].map(card => (
               <div
                 className={`card ${card}`}

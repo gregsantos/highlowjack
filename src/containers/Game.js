8f2a5b970b33b19807a642ac936b49d8795b3e7a
Greg Santos <gregsantos@gmail.com>
2020-04-04 16:47:52 -0700

    Add main grid and aside flex column

--- src/containers/Game.js
+++ src/containers/Game.js
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 import { jsx } from 'theme-ui'
-import { Container } from 'theme-ui'
+import { Container, Flex, Box } from 'theme-ui'
 import { FaUserSecret } from 'react-icons/fa'
 import { RoomWrapper } from '../components'
 import '../css/cards.css'
@@ -10,7 +10,7 @@ const GamePage = () => (
     <div
       sx={{
         display: 'flex',
-        flexDirection: ['column', 'column', 'row', 'row'],
+        flexDirection: ['column', 'column', 'column', 'row'],
         height: '100%',
         backgroundColor: 'black',
         border: 'solid',
@@ -21,16 +21,74 @@ const GamePage = () => (
         sx={{
           backgroundColor: 'red',
           height: ['100vw', '100vw', '100%', '100%'],
-          width: ['100%', '100%', '70%', '75%'],
+          width: ['100%', '100%', '100%', '75%'],
         }}
-      ></main>
+      >
+        <div
+          sx={{
+            display: 'grid',
+            gridGap: 2,
+            gridTemplateColumns: ['repeat(3, 1fr)'],
+            gridTemplateRows: ['repeat(3, 1fr)'],
+            height: '100%',
+            '& :nth-child(even)': {
+              backgroundColor: '#63B3ED',
+            },
+          }}
+        >
+          <div sx={{ justifySelf: 'start', alignSelf: 'start' }}>
+            <Container>
+              <FaUserSecret size='75%' />
+            </Container>
+          </div>
+          <div />
+          <div sx={{ justifySelf: 'end', alignSelf: 'start' }}>
+            <Container>
+              <FaUserSecret size='75%' />
+            </Container>
+          </div>
+          <div />
+          <Container>
+            <div
+              className='card hA  shadow no-border'
+              sx={{
+                fontSize: [2, 4, 6],
+              }}
+            />
+          </Container>
+          <div />
+          <div sx={{ justifySelf: 'start', alignSelf: 'end' }}>
+            <Container>
+              <FaUserSecret size='75%' />
+            </Container>
+          </div>
+          <div />
+          <div sx={{ justifySelf: 'end', alignSelf: 'end' }}>
+            <Container>
+              <FaUserSecret size='75%' />
+            </Container>
+          </div>
+        </div>
+      </main>
       <aside
         sx={{
           backgroundColor: 'blue',
           height: ['100vw', '80vw', '100%', '100%'],
           flexGrow: '1',
         }}
-      ></aside>
+      >
+        <Flex sx={{ flexDirection: 'column', height: '100%' }}>
+          <Box p={2} bg='green' sx={{ flex: '1 1 auto' }}>
+            Cards
+          </Box>
+          <Box p={2} bg='primary' sx={{ flex: '1 1 auto' }}>
+            Flex
+          </Box>
+          <Box p={2} bg='muted' sx={{ flex: '1 1 auto' }}>
+            Box
+          </Box>
+        </Flex>
+      </aside>
     </div>
   </RoomWrapper>
 )

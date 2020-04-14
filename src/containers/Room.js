845b57f69b56ab607b13e57b1bb5627b4517db83
Greg Santos <gregsantos@gmail.com>
2020-04-13 23:26:22 -0700

    Add tally trick

--- src/containers/Room.js
+++ src/containers/Room.js
@@ -91,7 +91,9 @@ const RoomPage = (props) => {
           if (lastCard) {
             return (
               <div
-                className={`card ${lastCard} shadow no-border`}
+                className={`card ${
+                  lastCard.suit + lastCard.spot
+                } shadow no-border`}
                 sx={{
                   fontSize: [3, 5, 6],
                 }}
@@ -114,24 +116,35 @@ const RoomPage = (props) => {
   const playCard = (i, card) => {
     const nextPlayer = gameData.turn === 3 ? 0 : gameData.turn + 1
     const lastPlayer = nextPlayer === gameData.leader
+    const cardArr = [...card]
+    const suit = cardArr.shift()
+    const spot = cardArr.join()
+
+    const cardData = {
+      p: playerSeat,
+      suit: suit,
+      spot: spot,
+    }
     const getUpdate = () => {
       if (!lastPlayer) {
         return {
           turn: nextPlayer,
-          trickCards: firebase.firestore.FieldValue.arrayUnion(card),
+          trickCards: firebase.firestore.FieldValue.arrayUnion(cardData),
         }
       } else {
         const tallyTrick = () => {
           const tallyRound =
-            gameData.trick === 7 && nextPlayer === gameData.leader
+            gameData.trick === 6 && nextPlayer === gameData.leader
           if (tallyRound) {
             // last trick in round
-            // check points toward bid, count for game and score round}
-            // high trump or boss leads
+            // call tallyRound() & resetRound()
+            // check points toward bid, count for game and score round
+            const nextDealer = gameData.dealer === 3 ? 0 : gameData.dealer + 1
             return {
-              // turn: calculateLeader(),
-              // leader: calculateLeader()
-              trick: gameData.trick + 1,
+              turn: nextDealer + 1,
+              leader: nextDealer + 1,
+              dealer: nextDealer,
+              trick: 0, // 7 for round results alert
               trickCards: [],
               t1RoundCards: [],
               t2RoundCards: [],
@@ -140,12 +153,28 @@ const RoomPage = (props) => {
             // last card in trick
             // calculate new leader, add trick to winners
             // roundCards
+            const trumped = gameData.trickCards.filter(
+              (c) => c.suit === gameData.bid.trumpSuit
+            )
+            const highTrump = trumped.sort((a, b) => b.spot - a.spot)
+            const suited = gameData.trickCards.filter(
+              (c) => c.suit === gameData.trickCards[0].suit
+            )
+            const highSuited = suited.sort((a, b) => b.spot - a.spot)
+            const winningCard = highTrump[0] || highSuited[0]
+            const handWinner = winningCard.p
+            const trickWinner = winningCard.p === 1 || 3 ? 0 : 1
             return {
-              // turn: calculateLeader(),
-              // leader: calculateLeader()
+              turn: handWinner,
+              leader: handWinner,
               trick: gameData.trick + 1,
               trickCards: [],
-              t1RoundCards: [],
+              t1RoundCards:
+                trickWinner === 0
+                  ? firebase.firestore.FieldValue.arrayUnion(
+                      ...[cardData, ...[gameData.trickCards]]
+                    )
+                  : [],
               t2RoundCards: [],
             }
           }
